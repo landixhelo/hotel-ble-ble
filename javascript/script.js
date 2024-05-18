4f453b5cdfd74a48bc23816cb5ab0829cf6516fe
landi <landixhelo02@gmail.com>
2024-05-18 11:48:11 +0200

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,4 @@
+const collapseElementList = document.querySelectorAll('.collapse')
+const collapseList = [...collapseElementList].map(collapseEl => new bootstrap.Collapse(collapseEl))
+
+document.getElementsByClassName("date").write(new Date().getFullYear()); 
